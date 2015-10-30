18802789526f5e2e986abaf8c2f3aab524a7a22a
keithconroy <kconroyjr@gmail.com>
2015-10-29 18:01:00 -0700

    larger text, better background

--- app/assets/javascripts/wishes.js
+++ app/assets/javascripts/wishes.js
@@ -4,35 +4,13 @@ $(document).on('page:change', function() {
   $('#newWish').find('input').focus();
 });
 
-// var colors = ["#fc4949", "#5684f7", "#58f4cd", "#f28557", "#fc7e7e", "#bf68f9"];
-
-function bindEvents(){
+var bindEvents = function(){
   $('#newWish').on('submit', makeAWish);
   $('#newWish').on('keyup', checkCharacters);
-  // $('#new_question_form').on('submit', newQuestion);
-  // $('#question_list').on('click', '.question_title', showQuestion);
-  // $('#question_list').on('click', '.delete', deleteQuestion);
   $('body').on('click', '.vote', voteWish);
-  // $('#search').on('keyup', search);
-
-  // $('body').on('submit', '.new_answer_form', newAnswer);
-  // $('body').on('click', '.answer_vote', voteAnswer);
 };
 
-// var getWishes = function(){
-//   var data = $(this).serialize();
-//   $.ajax({
-//     url: "wishes",
-//     type: 'get'
-//     }).done(function(data) {
-//     $('#wishes').html(data);
-//     // $('#item-count').text(data.count);
-//   }).fail(function() {
-//       console.log('error');
-//   });
-// };
-
-function getAllWishes(){
+var getAllWishes = function(){
   var source = $('#all_wishes').html();
   var template = Handlebars.compile(source);
   Handlebars.registerPartial("new_wish", $("#new_wish").html());
@@ -43,7 +21,6 @@ function getAllWishes(){
   }).done(function(wishes) {
     for(var wish in wishes){
       wishes[wish].created_at = moment(wishes[wish].created_at).fromNow();
-      // wishes[wish].color = colors[Math.floor(Math.random()*colors.length)];
     };
     context = {wishes: wishes}
     $('#wish_list').html(template(context))
@@ -58,16 +35,15 @@ var makeAWish = function(){
   var source = $('#new_wish').html();
   var template = Handlebars.compile(source);
   var data = $(this).serialize();
+
   $.ajax({
     url: "wishes",
     type: 'post',
     data: data
   }).done(function(wish) {
     wish.created_at = moment(wish.created_at).fromNow();
-    // wish.color = colors[Math.floor(Math.random()*colors.length)];
     $('#wish_list').prepend(template(wish));
     $('#newWish').find('input').val('');
-    // $('#item-count').text(data.count);
   }).fail(function() {
       console.log('error');
   });
@@ -78,29 +54,9 @@ var checkCharacters = function(){
   $('#wish-length').html(140 - length);
 }
 
-// function newQuestion(event){
-//   event.preventDefault();
-
-//   var source = $('#new_question').html();
-//   var template = Handlebars.compile(source);
-
-//   var data = $(this).serialize();
-
-//   $.ajax({
-//       url: 'http://localhost:3000/questions',
-//       type: 'post',
-//       data: data
-//   }).done(function(question) {
-//     $('#question_list').prepend(template(question))
-//     $('#form_title').val('');
-//     $('#form_content').val('');
-//   }).fail(function() {
-//       console.log('may your jimmies remain unrustled');
-//   });
-// }
-
-function voteWish(event){
+var voteWish = function(event){
   event.preventDefault();
+
   var link = $(this);
   var url = $(this).attr('href');
 
